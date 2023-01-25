d4281a42fb8a1cd58786d5dadbee93ddb7d51bc8
Dave <dolsavsky@gmail.com>
2023-01-25 16:52:55 +0000

    Created unit tests for backend services

---
+++ BugTracker.Services.Tests/BugServiceTests.cs
@@ -0,0 +1,229 @@
+﻿using NUnit.Framework;
+using Moq;
+using AutoFixture;
+using BugTracker.Services;
+using BugTracker.Mappers.Interfaces;
+using BugTracker.Repository.Interfaces;
+using BugTracker.Models;
+using BugTracker.Dtos;
+
+namespace BugTracker.Services.Tests;
+
+
+[TestFixture]
+public class BugServiceTests
+{
+    private BugService _bugService;
+
+    private Mock<IBugRepository> _bugRepository;
+    private Mock<IUserRepository> _userRepository;
+    private Mock<IBugMapper> _bugMapper;
+
+    private Fixture _fixture;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _fixture = new Fixture();
+
+        _bugRepository = new Mock<IBugRepository>();
+        _userRepository = new Mock<IUserRepository>();
+        _bugMapper = new Mock<IBugMapper>();
+
+        _bugService = new BugService(_bugRepository.Object, _bugMapper.Object, _userRepository.Object);
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        _bugMapper.Invocations.Clear();
+        _bugRepository.Invocations.Clear();
+        _userRepository.Invocations.Clear();
+    }
+
+    #region GetByPublicId
+
+    [Test]
+    public async Task GetByPublicId_BugNotFound_ResultIsNull()
+    {
+        //Arrange
+        var publicId = _fixture.Create<string>();
+
+        _bugRepository.Setup(x => x.GetByPublicId(It.IsAny<string>())).ReturnsAsync(() => null);
+
+        //Act
+        var result = await _bugService.GetByPublicId(publicId);
+
+        //Assert
+        Assert.IsNull(result);
+
+        _userRepository.Verify(x => x.GetById(It.IsAny<Guid>()), Times.Never);
+        _bugMapper.Verify(x => x.Map(It.IsAny<Bug>(), It.IsAny<User>()), Times.Never);
+    }
+
+    [Test]
+    public async Task GetByPublicId_BugHasUserId_RequestingUserOnce()
+    {
+        //Arrange
+        var publicId = _fixture.Create<string>();
+        var bug = _fixture.Create<Bug>();
+
+        _bugRepository.Setup(x => x.GetByPublicId(It.IsAny<string>())).ReturnsAsync(bug);
+
+        //Act
+        var result = await _bugService.GetByPublicId(publicId);
+
+        //Assert
+        _userRepository.Verify(x => x.GetById(It.IsAny<Guid>()), Times.Once);
+    }
+
+    [Test]
+    public async Task GetByPublicId_BugDoesNotHaveUserId_RequestingUserNever()
+    {
+        //Arrange
+        var publicId = _fixture.Create<string>();
+        var bug = _fixture.Build<Bug>().Without(b => b.UserId).Create();
+
+        _bugRepository.Setup(x => x.GetByPublicId(It.IsAny<string>())).ReturnsAsync(bug);
+
+        //Act
+        var result = await _bugService.GetByPublicId(publicId);
+
+        //Assert
+        _userRepository.Verify(x => x.GetById(It.IsAny<Guid>()), Times.Never);
+    }
+
+    [Test]
+    public async Task GetByPublicId_CallMapper_ResultIsMapperResponse()
+    {
+        //Arrange
+        var publicId = _fixture.Create<string>();
+        var bug = _fixture.Create<Bug>();
+        var user = _fixture.Create<User>();
+        var bugDto = _fixture.Create<BugDto>();
+
+        _bugRepository.Setup(x => x.GetByPublicId(It.IsAny<string>())).ReturnsAsync(bug);
+        _userRepository.Setup(x => x.GetById(It.IsAny<Guid>())).ReturnsAsync(user);
+        _bugMapper.Setup(x => x.Map(It.IsAny<Bug>(), It.IsAny<User>())).Returns(bugDto);
+
+        //Act
+        var result = await _bugService.GetByPublicId(publicId);
+
+        //Assert
+        Assert.Multiple(() => {
+            Assert.IsNotNull(result, "IsNotNull");
+            Assert.AreEqual(bugDto.AssigneeName, result.AssigneeName, "AssigneeName");
+            Assert.AreEqual(bugDto.Description, result.Description, "Description");
+            Assert.AreEqual(bugDto.OpenedOn, result.OpenedOn, "OpenedOn");
+            Assert.AreEqual(bugDto.PublicId, result.PublicId, "PublicId");
+            Assert.AreEqual(bugDto.Status, result.Status, "Status");
+            Assert.AreEqual(bugDto.Title, result.Title, "Title");
+            Assert.AreEqual(bugDto.Id, result.Id, "Id");
+            Assert.AreEqual(bugDto.UserId, result.UserId, "UserId");
+        });
+
+        _userRepository.Verify(x => x.GetById(It.IsAny<Guid>()), Times.Once);
+        _bugMapper.Verify(x => x.Map(It.IsAny<Bug>(), It.IsAny<User>()), Times.Once);
+    }
+
+    #endregion
+
+    #region CreateBug
+
+    [Test]
+    public async Task CreateBug_RepositoryReturnsNull_ResultIsNull()
+    {
+        //Arrange
+        var request = _fixture.Create<CreateBug>();
+
+        _bugRepository.Setup(x => x.CreateBug(It.IsAny<CreateBug>())).ReturnsAsync(() => null);
+
+        //Act
+        var result = await _bugService.CreateBug(request);
+
+        //Assert
+        Assert.IsNull(result);
+    }
+
+    [Test]
+    public async Task CreateBug_RepositoryReturnsBugModel_ResultIsBugModel()
+    {
+        //Arrange
+        var request = _fixture.Create<CreateBug>();
+        var bugResponse = _fixture.Create<Bug>();
+
+        _bugRepository.Setup(x => x.CreateBug(It.IsAny<CreateBug>())).ReturnsAsync(bugResponse);
+
+        //Act
+        var result = await _bugService.CreateBug(request);
+
+        //Assert
+        Assert.Multiple(() => {
+            Assert.NotNull(result, "NotNull");
+            Assert.AreEqual(bugResponse.Id, result.Id, "Id");
+            Assert.AreEqual(bugResponse.PublicId, result.PublicId);
+            Assert.AreEqual(bugResponse.Title, result.Title);
+            Assert.AreEqual(bugResponse.Status, result.Status);
+            Assert.AreEqual(bugResponse.Description, result.Description);
+            Assert.AreEqual(bugResponse.OpenedOn, result.OpenedOn);
+            Assert.AreEqual(bugResponse.UserId, result.UserId);
+        });
+    }
+
+    #endregion
+
+    #region GetAllBugs
+
+    [Test]
+    public async Task GetAllBugs_RepositoryReturnsNull_ResultIsNull()
+    {
+        //Arrange
+        _bugRepository.Setup(x => x.GetAllBugs()).ReturnsAsync(() => null);
+
+        //Act
+        var result = await _bugService.GetAllBugs();
+
+        //Assert
+        Assert.IsNull(result);
+    }
+
+    [Test]
+    public async Task GetAllBugs_RepositoryReturnsListOfBugsModel_ResultIsListOfBugsModel()
+    {
+        //Arrange
+        var bugs = _fixture.CreateMany<Bug>();
+
+        _bugRepository.Setup(x => x.GetAllBugs()).ReturnsAsync(bugs);
+
+        //Act
+        var result = await _bugService.GetAllBugs();
+
+        //Assert
+        Assert.Multiple(() => {
+            Assert.NotNull(result, "NotNull");
+            Assert.AreEqual(bugs.Count(), result.Count(), "Count");
+        });
+    }
+
+    #endregion
+
+    #region UpdateBugStatus
+
+    [TestCase(true)]
+    [TestCase(false)]
+    public async Task UpdateBugStatus_ResultIsResponseFromRepository(bool repositoryResponse)
+    {
+        //Arrange
+        var request = _fixture.Create<BugStatusUpdate>();
+
+        _bugRepository.Setup(x => x.UpdateBugStatus(It.IsAny<BugStatusUpdate>())).ReturnsAsync(repositoryResponse);
+
+        //Act
+        var result = await _bugService.UpdateBugStatus(request);
+
+        //Assert
+        Assert.AreEqual(repositoryResponse, result);
+    }
+
+    #endregion
+}
---
+++ BugTracker.Services.Tests/UserServiceTests.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using NUnit.Framework;
+using Moq;
+using AutoFixture;
+using BugTracker.Mappers.Interfaces;
+using BugTracker.Repository.Interfaces;
+using BugTracker.Models;
+using BugTracker.Mappers;
+
+namespace BugTracker.Services.Tests
+{
+    [TestFixture]
+    public class UserServiceTests
+	{
+        private UserService _userService;
+        private Mock<IUserRepository> _userRepository;
+        private Fixture _fixture;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _fixture = new Fixture();
+
+            _userRepository = new Mock<IUserRepository>();
+
+            _userService = new UserService(_userRepository.Object);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _userRepository.Invocations.Clear();
+        }
+
+        #region CreateUser
+
+        [Test]
+        public async Task CreateUser_RepositoryReturnsUser_ResultIsUser()
+        {
+            //Arrange
+            var name = _fixture.Create<string>();
+            var user = _fixture.Create<User>();
+
+            _userRepository.Setup(x => x.Create(It.IsAny<string>())).ReturnsAsync(user);
+
+            //Act
+            var result = await _userService.CreateUser(name);
+
+            //Assert
+            Assert.Multiple(() => {
+                Assert.IsNotNull(result, "IsNotNull");
+                Assert.IsInstanceOf<User>(result, "IsInstanceOf");
+                Assert.AreEqual(user.Id, result.Id, "Id");
+                Assert.AreEqual(user.Name, result.Name, "Name");
+            });
+
+            _userRepository.Verify(x => x.Create(It.IsAny<string>()), Times.Once);
+        }
+
+        [Test]
+        public async Task CreateUser_RepositoryReturnsNull_ResultIsNull()
+        {
+            //Arrange
+            var name = _fixture.Create<string>();
+
+            _userRepository.Setup(x => x.Create(It.IsAny<string>())).ReturnsAsync(() => null);
+
+            //Act
+            var result = await _userService.CreateUser(name);
+
+            //Assert
+            Assert.IsNull(result);
+
+            _userRepository.Verify(x => x.Create(It.IsAny<string>()), Times.Once);
+        }
+
+        #endregion
+
+        #region GetAll
+
+        [Test]
+        public async Task GetAll_RepositoryReturnsNull_ResultIsNull()
+        {
+            //Arrange
+            _userRepository.Setup(x => x.GetAll()).ReturnsAsync(() => null);
+
+            //Act
+            var result = await _userService.GetAll();
+
+            //Assert
+            Assert.IsNull(result);
+
+            _userRepository.Verify(x => x.GetAll(), Times.Once);
+        }
+
+        [Test]
+        public async Task GetAll_RepositoryReturnsListOfUsers_ResultIsListOfUsers()
+        {
+            //Arrange
+            var users = _fixture.CreateMany<User>();
+
+            _userRepository.Setup(x => x.GetAll()).ReturnsAsync(users);
+
+            //Act
+            var result = await _userService.GetAll();
+
+            //Assert
+            Assert.Multiple(() => {
+                Assert.IsNotNull(result, "IsNotNull");
+                Assert.IsInstanceOf<IEnumerable<User>>(result, "IsInstanceOf");
+                Assert.AreEqual(users.Count(), result.Count(), "Count");
+            });
+
+            _userRepository.Verify(x => x.GetAll(), Times.Once);
+        }
+
+        #endregion
+
+        #region UpdateUser
+
+        [TestCase(true)]
+        [TestCase(false)]
+        public async Task UpdateUser_ResultIsRepositoryResponse(bool repositoryResponse)
+        {
+            //Arrange
+            var user = _fixture.Create<User>();
+
+            _userRepository.Setup(x => x.UpdateUser(It.IsAny<User>())).ReturnsAsync(repositoryResponse);
+
+            //Act
+            var result = await _userService.UpdateUser(user);
+
+            //Assert
+            Assert.Multiple(() => {
+                Assert.IsNotNull(result, "IsNotNull");
+                Assert.IsInstanceOf<bool>(result, "IsInstanceOf");
+                Assert.AreEqual(repositoryResponse, result, "Count");
+            });
+
+            _userRepository.Verify(x => x.UpdateUser(It.IsAny<User>()), Times.Once);
+        }
+
+        #endregion
+    }
+}
+
